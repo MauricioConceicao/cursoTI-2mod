9e360a5b92cd25ce35e4af087811d1c5d94c21cb
Mauricio Conceição <58689369+MauricioConceicao@users.noreply.github.com>
2023-03-06 20:35:00 -0300

    05/03/2023-Aula de Linguagem C
    Na aula de hoje, A professora mostrou os comandos básicos para implementação de entrada e saída de dados.

---
+++ Línguagem de programação-C/codigo de DEVC++/Aula2/mostrar nome e idade com variavel.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h> //STDIO.H e uma biblioteca responsavel por interpretar comandos de entrada e saida.
+
+
+main()
+
+{
+	int idade;
+	printf("Meu nome e mauricio saturnino da conceicao.\n");
+	printf("de a idade:");
+	scanf("%d",&idade);
+	printf("\n eu tenho %d",idade);
+}
